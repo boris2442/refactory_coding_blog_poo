4091c8b5313ba79048c43c7c9e0d4a24519e516d
boris2442 <aubinborissimotsebo@gmail.com>
2025-05-27 10:56:47 +0200

    singleton

--- libraries/database.php
+++ libraries/database.php
@@ -1,28 +1,27 @@
 <?php
 // $pdo=Database::getPdo(); //ici, il s'agit d'une methode statique, on peut l'appeler sans instancier la classe Database
 //Le singleton permet de limiter les connexions a la bases de donnees 
- $model=new \Models\Comment();
-$commentaires=$model->findALl();  // premiere connexion a mysql
- $commentaire->$model->find(); //deuxieme connexion a mysql
- $commentaire->$model->delete(); //troisieme connexion a mysql
+$model = new \Models\Comment();
+$commentaires = $model->findALl();  // premiere connexion a mysql
+$commentaire->$model->find(); //deuxieme connexion a mysql
+$commentaire->$model->delete(); //troisieme connexion a mysql
 class Database
 {
     private static  $instance = null; //pour eviter de creer plusieurs instances de la classe Database, on utilise le singleton
 
-//si une propriete est statique, on l'appelle avec self
+    //si une propriete est statique, on l'appelle avec self
     /**
      * Retourne une connexion a la database
-     * @return PDO
+     * @return PDO: PHP DATA OBJECT
      */
     public static  function getPdo(): PDO
     {
-        if(self::$instance !== null) {
-            return self::$instance; //si l instance existe deja, on la retourne
+        if (self::$instance == null) { //je n'est pas encore d'instance
+            self::$instance = new PDO('mysql:host=localhost; dbname=blogphp-2025.sql; charset=utf8', 'root', '', [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //quand il ya une erreur  je veux que tu affiches
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC //on doit exploiter les donees sous forme de tableau associatif
+            ]);
         }
-        $pdo = new PDO('mysql:host=localhost; dbname=blogphp-2025.sql; charset=utf8', 'root', '', [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //quand il ya une erreur  je veux que tu affiches
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC //on doit exploiter les donees sous forme de tableau associatif
-        ]);
-        return $pdo;
+        return self::$instance;
     }
 }
